8ba5634595fe7aafd99b7f91e1b58d93d4b03069
didzl89 <limjihwan89@naver.com>
2014-06-07 14:42:04 +0900

    erase warnings

--- src/com/helloworld/freshmarketapp/MainActivity.java
+++ src/com/helloworld/freshmarketapp/MainActivity.java
@@ -3,30 +3,23 @@ package com.helloworld.freshmarketapp;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
-import android.os.Build;
 
 public class MainActivity extends FragmentActivity {
 	 
