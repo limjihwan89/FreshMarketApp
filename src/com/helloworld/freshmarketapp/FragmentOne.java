b6a1f355cc438d74c831cd2a70a3d64052fcee65
didzl89 <limjihwan89@naver.com>
2014-06-08 16:39:41 +0900

    spinner

--- src/com/helloworld/freshmarketapp/FragmentOne.java
+++ src/com/helloworld/freshmarketapp/FragmentOne.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
  
 public class FragmentOne extends Fragment {
 	ImageView ivIcon;
     TextView tvItemName;
+    Spinner spinner;
 
     public static final String IMAGE_RESOURCE_ID = "iconResourceID";
     public static final String ITEM_NAME = "itemName";
@@ -26,6 +29,11 @@ public class FragmentOne extends Fragment {
 
   	  View view = inflater.inflate(R.layout.fragment_layout_one, container,
                 false);
+  	  spinner = (Spinner) view.findViewById(R.id.spinner1);
+  	  ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.categories, android.R.layout.simple_spinner_item);
+  	  adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+  	  spinner.setAdapter(adapter);
     return view;
     }
+    
 }
